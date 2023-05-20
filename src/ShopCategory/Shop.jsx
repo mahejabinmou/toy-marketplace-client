559b0d0a4aa9a2b2cc15519cb27e3fab277a3cea
Mou <mahejabinmou.99@gmail.com>
2023-05-20 09:35:35 -0700

    card added on tab

--- src/ShopCategory/Shop.jsx
+++ src/ShopCategory/Shop.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
+import React from 'react';
 
-// const Shop = ({ toy }) => {
-//     const { img, toy_name,
-//         price, Rating, } = { toy };
-//     return (
-//         <div className="card w-96 bg-base-100 shadow-xl">
-//             <figure className="px-10 pt-10">
-//                 <img src={img} alt="Shoes" className="rounded-xl" />
-//             </figure>
-//             <div className="card-body items-center text-center">
-//                 <h2 className="card-title">{toy_name}</h2>
-//                 <p>{price}</p>
-//                 <p>{Rating}</p>
-//                 <div className="card-actions">
-//                     <button className="btn btn-primary">View details</button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const Shop = ({ toy }) => {
+    const { img, toy_name,
+        price, Rating, } = { toy };
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{toy_name}</h2>
+                <p>{price}</p>
+                <p>{Rating}</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default Shop;
+export default Shop;
